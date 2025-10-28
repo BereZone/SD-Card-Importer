e0195abbf94c95fbd04bc0661401613be31dd4cf
BereZone <79885796+BereZone@users.noreply.github.com>
2025-10-28 17:31:45 -0400

    Initial Commit

---
+++ SD Card File Importer/SD_Card_File_ImporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SD_Card_File_ImporterApp.swift
+//  SD Card File Importer
+//
+//  Created by Albert Sheng on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SD_Card_File_ImporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
